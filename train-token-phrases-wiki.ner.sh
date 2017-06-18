beb0ac0382d3bdacd89ad82125b692187244f981
Jason Chen <z.w.chan.jason@gmail.com>
2017-06-18 19:16:31 -0400

    save scripts for generating result

---
+++ train-token-phrases-wiki.ner.sh
@@ -0,0 +1,5 @@
+make
+time ~/data/code/word2vec_commented/word2phrase -train ~/data/wikidata/depth4/token.ner.txt  -output ~/data/wikidata/depth4/token.ner-p2.txt -threshold 200 -showNumber 1000000 -debug 2 > ~/data/wikidata/depth4/token.ner-vectors.phrase2
+time ~/data/code/word2vec_commented/word2phrase -train ~/data/wikidata/depth4/token.ner-p2.txt  -output ~/data/wikidata/depth4/token.ner-p4.txt -threshold 100 -showNumber 1000000 -debug 2 > ~/data/wikidata/depth4/token.ner-vectors.phrase4
+time ~/data/code/word2vec_commented/word2vec -train ~/data/wikidata/depth4/token.ner-p4.txt -output ~/data/wikidata/depth4/token.ner-vectors-phrase.bin -cbow 0 -size 300 -window 5 -negative 5 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15  -save-vocab  ~/data/wikidata/depth4/token.ner-vectors-phrase.vocab
+#./distance vectors-phrase.bin
